55120bbb758495c028e72364a239fbf51288d8e8
pachevang <vangpache@gmail.com>
2019-08-14 08:58:17 -0500

    adds if statement

--- src/App.js
+++ src/App.js
@@ -11,25 +11,26 @@ class App extends Component {
 
   handleButton = (event) => {
     console.log('add run button was clicked:',this.state);
+    // this.setState({mostRecent: this.mostRecent})
 
+    if(this.state.mostRecent > this.state.longestRun) {
+      return this.setState({longestRun: this.state.mostRecent})
+    }
   }
 
 
     handleMiles = (event) => {
     console.log(event.target.value);
     this.setState ({mostRecent: event.target.value })
-    console.log(this.state);
-    
+    // console.log(this.state); 
   }
-
-
 
 
   render() {
     return (
       <div className="App">
        <h1>React Track Runner</h1>
-        <input type="number" placeholder="Miles Ran" onChange={this.handleMiles} />
+        <input className ="milesInput" type="number" placeholder="Miles Ran" onChange={this.handleMiles} />
         <button onClick={this.handleButton}>Add New Run</button>
        <p>Recent Run: {this.state.mostRecent}</p>
         <p>Longest Run:  {this.state.longestRun}</p>
